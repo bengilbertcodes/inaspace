e7cfc65fbb53c684fe6774cb3bd36d1af1b65391
Ben <144111469+bengilbertcodes@users.noreply.github.com>
2024-07-20 01:23:04 +0100

    remove db model

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,12 +1,2 @@
 from django.contrib import admin
-from .models import RoomType, Room, TimeSlot, Booking
 
-class RoomAdmin(admin.ModelAdmin):
-	list_display = ('name', 'room_type', 'location', 'capacity')
-	search_fields = ('name', 'location')
-	list_filter = ('room_type',)
-
-admin.site.register(RoomType)
-admin.site.register(Room, RoomAdmin)
-admin.site.register(TimeSlot)
-admin.site.register(Booking)
