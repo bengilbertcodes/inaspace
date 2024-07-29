49a9c4b1f5170b1ca8dbe15ed8a85d93a89846e6
Ben <144111469+bengilbertcodes@users.noreply.github.com>
2024-07-29 15:07:47 +0100

    Fixed home page for unauthenticated users

--- home/views.py
+++ home/views.py
@@ -1,14 +1,25 @@
+# home/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from bookings.models import Booking
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            # Allow unauthenticated users to access the home page
+            return super().dispatch(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        context['bookings'] = Booking.objects.filter(user=self.request.user)
+        if self.request.user.is_authenticated:
+            context['bookings'] = Booking.objects.filter(
+                user=self.request.user)
+        else:
+            # Different view for unauthenticated users
+            context['bookings'] = []
         return context
