abcb55d7c9557ae25b238870e4fe789df7c02217
Ben <144111469+bengilbertcodes@users.noreply.github.com>
2024-07-30 22:28:38 +0100

    Bookings less than today hidden from HomeView

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
 from bookings.models import Booking
 
 
@@ -16,9 +17,17 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated:
+            now = timezone.now()
             context['bookings'] = Booking.objects.filter(
-                user=self.request.user)
+                user=self.request.user,
+                date__gte=now.date(),
+			)
+				
         else:
             # Different view for unauthenticated users
             context['bookings'] = []
         return context
+
+
+class ActiveBookingsView(TemplateView):
+    template_name = 'home/home.html'
