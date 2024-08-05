57673e6bd82377a45d32e7cb2ad5f4df23d020f1
Ben <144111469+bengilbertcodes@users.noreply.github.com>
2024-08-05 08:38:39 +0100

    remove trailing comma

--- bookings/forms.py
+++ bookings/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from allauth.account.forms import SignupForm
 from django.core.exceptions import ValidationError
 from .models import Booking
-from datetime import datetime, timedelta,
+from datetime import datetime, timedelta
 
 
 class CustomSignupForm(SignupForm):
